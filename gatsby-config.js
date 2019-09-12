35771d27a690a3a59894377f0451cb88e59fd258
AleksanderSkubala <olek_s@wtvk.pl>
2019-09-12 20:40:32 +0200

    Create starter app

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `zskTasks`,
+    description: `School app for aggregating tasks.`,
+    author: `@mbdjj, @datejer, @AdamSiekierski, @AleksanderSkubala`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `zskTasks`,
+        short_name: `zskTasks`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#28146D`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
